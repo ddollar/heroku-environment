8624f7d38e08d3c11e229197f6a94fd44fcb4a73
David Dollar <ddollar@gmail.com>
2010-10-14 14:45:27 -0400

    cleaner operation in both directions, optional --overwrite

--- lib/heroku/command/config.rb
+++ lib/heroku/command/config.rb
@@ -3,9 +3,17 @@ require "yaml"
 class Heroku::Command::Config < Heroku::Command::BaseWithApp
 
   def pull
+    existing_config = YAML.load_file(".environment") rescue {}
+    existing_keys = existing_config.keys
+
     File.open(".environment", "w") do |file|
-      file.puts YAML.dump(heroku.config_vars(app))
+      config = heroku.config_vars(app)
+      existing_keys.each { |k| config.delete(k) } if extract_option("--new")
+      output = YAML.dump(existing_config.merge(config))
+      file.puts "---"
+      file.puts output.split("\n")[1..-1].sort.join("\n")
     end
+
     display "Config for #{app} written to .environment"
   end
 
