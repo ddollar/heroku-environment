a52f33e8bdcab158dcb0c248bdf4ab91cb4710e3
David Dollar <ddollar@gmail.com>
2011-02-01 13:29:48 -0500

    allow for interactive merges

--- lib/heroku/command/config.rb
+++ lib/heroku/command/config.rb
@@ -3,15 +3,19 @@ require "yaml"
 class Heroku::Command::Config < Heroku::Command::BaseWithApp
 
   def pull
-    overwrite = extract_option("--overwrite")
-    config = merge_config(remote_config, local_config, overwrite)
+    interactive = extract_option("--interactive")
+    overwrite   = extract_option("--overwrite")
+
+    config = merge_config(remote_config, local_config, interactive, overwrite)
     write_local_config config
     display "Config for #{app} written to .environment"
   end
 
   def push
-    overwrite = extract_option("--overwrite")
-    config = merge_config(local_config, remote_config, overwrite)
+    interactive = extract_option("--interactive")
+    overwrite   = extract_option("--overwrite")
+
+    config = merge_config(local_config, remote_config, interactive, overwrite)
     write_remote_config config
     display "Config in .environment written to #{app}"
   end
@@ -38,8 +42,17 @@ private ######################################################################
     heroku.add_config_vars(app, config)
   end
 
-  def merge_config(source, target, overwrite=false)
-    overwrite ? target.merge(source) : source.merge(target)
+  def merge_config(source, target, interactive=false, overwrite=false)
+    if interactive
+      source.keys.sort.inject(target) do |hash, key|
+        value = source[key]
+        display "%s: %s" % [key, value]
+        hash[key] = value if confirm("Overwite? (y/N)")
+        hash
+      end
+    else
+      overwrite ? target.merge(source) : source.merge(target)
+    end
   end
 
 end
